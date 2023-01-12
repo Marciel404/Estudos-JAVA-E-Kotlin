fbc2f513846fcfae45209165f23fdf44d404fc51
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-01-12 17:49:48 -0300

    Update Configs.java

--- src/main/java/com/github/marciel404/bot/utils/Configs.java
+++ src/main/java/com/github/marciel404/bot/utils/Configs.java
@@ -1,11 +1,12 @@
 package com.github.marciel404.bot.utils;
 
 public interface Configs {
+    
     String token = "Token bot Here";
+    
     String mongokey ="Mongo Token Here";
+    
     String database = "Name DataBase Here";
 
-    long[] channels = {
-            //Staff
-            1044742161238855811L};
+    long[] channels = {};
 }
